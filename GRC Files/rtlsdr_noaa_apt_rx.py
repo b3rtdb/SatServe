b54583f229c01eab41fb3f0843aead64bd867414
b3rtdb <bertdb@gmx.com>
2023-03-22 15:27:26 +0100

    change back to soapy

--- GRC Files/rtlsdr_noaa_apt_rx.py
+++ GRC Files/rtlsdr_noaa_apt_rx.py
@@ -21,7 +21,7 @@ import signal
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-from gnuradio import SoapySDR
+from gnuradio import soapy
 
 
 
